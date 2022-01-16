8873358dc3f37fec0e94a90a9aea7fd5db784db8
akonst17 <akonst17@gmail.com>
2022-01-16 21:19:34 +0300

    add user manager

---
+++ OtterScaleiOS/User/Request/UserSetRequest/UserSetRequest.swift
@@ -0,0 +1,44 @@
+//
+//  UserSetRequest.swift
+//  OtterScaleiOS
+//
+//  Created by Андрей Чернышев on 16.01.2022.
+//
+
+struct UserSetRequest: EndPoint {
+    let apiKey: String
+    let anonymousID: String
+    let externalUserID: String?
+    let otterScaleUserID: String?
+    let userParameters: [String: Any]
+    
+    var path: String {
+        "/api/v1/sdk/set"
+    }
+    
+    var method: HTTPMethod {
+        .post
+    }
+    
+    var parameters: [String : Any] {
+        var params: [String: Any] = [
+            "_api_key": apiKey,
+            "anonymous_id": anonymousID,
+            "parameters": userParameters
+        ]
+        
+        if let externalUserID = externalUserID {
+            params["external_user_id"] = externalUserID
+        }
+        
+        if let otterScaleUserID = otterScaleUserID {
+            params["otterscale_user_id"] = otterScaleUserID
+        }
+        
+        return params
+    }
+    
+    var headers: [String : String] {
+        [:]
+    }
+}
---
+++ OtterScaleiOS/User/UserManager.swift
@@ -0,0 +1,63 @@
+//
+//  UserManager.swift
+//  OtterScaleiOS
+//
+//  Created by Андрей Чернышев on 16.01.2022.
+//
+
+protocol UserManagerProtocol {
+    func set(userID: String,
+             mapper: UserSetResponseProtocol,
+             completion: ((Bool) -> Void)?)
+}
+
+final class UserManager: UserManagerProtocol {
+    private let apiEnvironment: APIEnvironmentProtocol
+    private var storage: StorageProtocol
+    private let requestDispatcher: RequestDispatcherProtocol
+    
+    private lazy var operations = [String: APIOperation]()
+    
+    init(apiEnvironment: APIEnvironmentProtocol,
+         storage: StorageProtocol) {
+        self.apiEnvironment = apiEnvironment
+        self.storage = storage
+        self.requestDispatcher = RequestDispatcher(environment: apiEnvironment,
+                                                  networkSession: NetworkSession())
+    }
+}
+
+// MARK: Internal
+extension UserManager {
+    func set(userID: String,
+             mapper: UserSetResponseProtocol = UserSetResponse(),
+             completion: ((Bool) -> Void)?) {
+        let userParameters = [
+            "external_id": userID
+        ]
+        let request = UserSetRequest(apiKey: apiEnvironment.apiKey,
+                                     anonymousID: storage.anonymousID,
+                                     externalUserID: storage.externalUserID,
+                                     otterScaleUserID: storage.otterScaleUserID,
+                                     userParameters: userParameters)
+        let operation = APIOperation(endPoint: request)
+        
+        let key = "set_user_id"
+        
+        operations[key] = operation
+        
+        operation.execute(dispatcher: requestDispatcher) { [weak self] response in
+            if let response = response {
+                let result = mapper.map(response: response)
+                
+                self?.storage.externalUserID = userID
+                
+                completion?(result)
+            } else {
+                completion?(false)
+            }
+            
+            self?.operations.removeValue(forKey: key)
+        }
+    }
+}
